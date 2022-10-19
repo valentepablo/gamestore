0c2df8881816fdca58a4d25e4c5aca9989536099
Pablo Valente <pablo.nvalente@gmail.com>
2022-10-19 00:24:50 -0300

    fixed li items on navbar

--- src/components/navigationBar/LinkList.jsx
+++ src/components/navigationBar/LinkList.jsx
@@ -25,23 +25,21 @@ const LinkList = ({ menuOpen, setMenuOpen }) => {
       className={`${
         menuOpen ? '' : '-translate-x-full md:translate-x-0'
       } top-[50px] absolute px-4 transition transform md:px-0 divide-y md:divide-none md:h-auto shadow-md md:shadow-none divide-zinc-700/20 left-0 w-full my-6 md:z-auto z-[1] md:static md:my-0 md:flex md:items-center md:w-auto bg-zinc-900`}>
-      <li className='py-5 pt-8 md:py-0 md:my-0' key='all-games' onClick={handleMenuOpen}>
-        <Link
-          className='text-lg font-semibold tracking-widest uppercase select-none md:text-sm md:mx-4 hover:text-sky-500'
-          to='/'>
-          All Games
-        </Link>
-      </li>
+      <Link
+        key='all-games'
+        className='block py-5 text-lg font-semibold tracking-widest uppercase select-none md:py-0 md:my-0 md:text-sm md:mx-4 hover:text-sky-500'
+        to='/'>
+        <li onClick={handleMenuOpen}>All Games</li>
+      </Link>
 
       {categories.map((category) => {
         return (
-          <li className='py-5 md:my-0 md:py-0' key={category.id} onClick={handleMenuOpen}>
-            <Link
-              className='text-lg font-semibold tracking-widest uppercase select-none md:text-sm md:mx-4 hover:text-sky-500'
-              to={`/category/${category.categoryName}`}>
-              {category.categoryName}
-            </Link>
-          </li>
+          <Link
+            key={category.id}
+            className='block py-5 text-lg font-semibold tracking-widest uppercase select-none md:text-sm md:mx-4 md:my-0 md:py-0 hover:text-sky-500'
+            to={`/category/${category.categoryName}`}>
+            <li onClick={handleMenuOpen}>{category.categoryName}</li>
+          </Link>
         );
       })}
       {menuOpen && (
